f3e00aa26f49c43edfde13f516a22c8279ee44dd
harsha711 <chinna.puvvadi@gmail.com>
2022-01-05 21:45:10 +0530

    Updates

---
+++ precache-manifest.6ff699f099bc04fd7a176851a7da9626.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "876c191775072cd737e8",
+    "url": "/kalpas-assessment/static/css/main.75d63dd9.chunk.css"
+  },
+  {
+    "revision": "876c191775072cd737e8",
+    "url": "/kalpas-assessment/static/js/main.876c1917.chunk.js"
+  },
+  {
+    "revision": "e5eaf0fb7afe3f548b14",
+    "url": "/kalpas-assessment/static/css/1.0f44324f.chunk.css"
+  },
+  {
+    "revision": "e5eaf0fb7afe3f548b14",
+    "url": "/kalpas-assessment/static/js/1.e5eaf0fb.chunk.js"
+  },
+  {
+    "revision": "0b7fe41ae4d12f4c7547",
+    "url": "/kalpas-assessment/static/js/runtime~main.0b7fe41a.js"
+  },
+  {
+    "revision": "03775ac8908e4fd49992a8c2ee78bd2a",
+    "url": "/kalpas-assessment/index.html"
+  }
+];
